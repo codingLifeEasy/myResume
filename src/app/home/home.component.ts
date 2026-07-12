a659b6cf9f4c0a64ae71f7564c5116a050a84db0
codingLifeEasy <subratadasworking@gmail.com>
2026-07-12 10:32:03 +0530

    feat: redesign home hero with typing effect and DE focus

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-    title = 'Portfolio';
- 
-  fullText = 'Hi !!! Recruter my name is SUBRATA DAS';
-  displayedText = '';
-  showDots = false;
+export class HomeComponent implements OnInit, OnDestroy {
+  title = 'Data Engineer Portfolio';
+  displayText = '';
+  fullTexts = ['Data Engineer', 'PySpark Developer', 'Azure Databricks Specialist', 'ETL Pipeline Architect'];
+  textIndex = 0; charIndex = 0; isDeleting = false;
+  private typingTimer: any;
 
-  ngOnInit(): void {
-    this.animateText();
-  }
+  ngOnInit(): void { this.startTyping(); }
+  ngOnDestroy(): void { if (this.typingTimer) clearTimeout(this.typingTimer); }
 
-  animateText() {
-    let i = 0;
-    const interval = setInterval(() => {
-      if (i < this.fullText.length) {
-        this.displayedText += this.fullText.charAt(i);
-        i++;
-      } else {
-        clearInterval(interval);
-        this.showDots = true;
+  startTyping() {
+    const currentText = this.fullTexts[this.textIndex];
+    const speed = this.isDeleting ? 50 : 100;
+    if (!this.isDeleting) {
+      this.displayText = currentText.substring(0, this.charIndex + 1);
+      this.charIndex++;
+      if (this.charIndex === currentText.length) {
+        this.isDeleting = true;
+        this.typingTimer = setTimeout(() => this.startTyping(), 2000);
+        return;
       }
-    }, 150); // adjust speed here
+    } else {
+      this.displayText = currentText.substring(0, this.charIndex - 1);
+      this.charIndex--;
+      if (this.charIndex === 0) {
+        this.isDeleting = false;
+        this.textIndex = (this.textIndex + 1) % this.fullTexts.length;
+      }
+    }
+    this.typingTimer = setTimeout(() => this.startTyping(), speed);
   }
-
 }
